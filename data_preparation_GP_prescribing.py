ff213b0d1a4a9c327f6d005c9949baff07085923
HolgerKunz <rmhihku@ad.ucl.ac.uk>
2017-11-02 11:54:16 +0000

    update

--- data_preparation_GP_prescribing.py
+++ data_preparation_GP_prescribing.py
@@ -17,7 +17,7 @@ prefixes=[]
 files=[]
 
 import calendar
-for year in range(2014,2017):
+for year in range(2015,2017):
     for i in range(1,13):
         prefixes.append(str(year) +'_' +str(i).zfill(2) + '_' + calendar.month_name[i] + '/')
         files.append('T' + str(year) +str(i).zfill(2) + 'PDPI+BNFT.CSV')
@@ -25,14 +25,18 @@ for year in range(2014,2017):
   
 #read files and reduce size
 
+df_small = pd.DataFrame()
+
+
+
 def file_reduction(prefix, file):
     df =  pd.read_csv(path + prefix +file, header=None, names=cols2, index_col=False, skiprows=1).rename(columns=lambda x: x.strip())
     df.practice=df.practice.astype('str')
     df_small= df[(df['practice'].isin(sub_df['GP_PRACTICE_CODE']) == True)] #only proportion of entire dataset
-    df_small.to_csv('N:\\Work\\DMHR2017-18\\visualization\\data\\' + file)
+    df_small.to_csv('N:\\Work\\DMHR2017-18\\visualization\\data\\' + file, index=False)
     del df #make space in memory
     return None
-    
+
     
 for prefix, file in zip(prefixes,files):
     print(path + prefix +file)
